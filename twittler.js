a462897d2912cdb76aed7f5e6de917d946963769
shashi-dokania <shashi.dokania@gmail.com>
2015-08-28 23:23:29 -0700

    added user tweet functionality

--- twittler.js
+++ twittler.js
@@ -1,7 +1,7 @@
 var count = 0;
 
 // creating new tweets in intervals and creating a count for number of tweets
-var nextTweet = function(){
+var nextTweet = function() {
   generateRandomTweet();
   count++;
   $("#new-tweet").show();
@@ -9,9 +9,15 @@ var nextTweet = function(){
   setTimeout(nextTweet, Math.random() * 5000);
 };
 
+//create a pop-up form on click of the button to allow user to tweet
+function createPopUp() {
+
+};
+
 // creating the format in which time elapsed to be displayed
 function format_time(value, type_str) {
   var str = "";
+  
   if (value > 1) {
     str = value.toString() + " " + type_str + "s" + " ago";
   }
@@ -19,7 +25,7 @@ function format_time(value, type_str) {
     str = value.toString() + " " + type_str + " ago";
   }
   return str;
-}
+};
 
 //calculating the elapsed time
 function elapsedTime(time_created) {
@@ -48,13 +54,13 @@ function elapsedTime(time_created) {
 } 
 
 //streaming the tweets along with timestamps
-function streamTweet(user){
+function streamTweet(user) {
   $("#new-tweet").hide();
   count = 0;
   var $body = $('.tweet-container');
   $body.html('');
   var index = 0;
-  
+
   if (user === "home") {
     index = streams.home.length - 1;
   }
@@ -72,13 +78,13 @@ function streamTweet(user){
 
     var $tweet = $('<div id=block class=well></div>');
     var $name = $('<a id='+tweet.user+'></a>');
-    var $time = $('<small id=time-date></small>')
-    var $timelapsed = $('<small id=time></small><br>')
+    var $time = $('<small id=time-date></small>');
+    var $timelapsed = $('<small id=time></small><br>');
     var $message = $('<p></p>');
-    
+
     $name.text('@' + tweet.user + ': ');
     $name.appendTo($tweet);
-    
+
     var date = new Date(tweet.created_at);
     $time.text(' posted on ' + (date.getMonth()+1) + "/"
       + date.getDate()  + "/" 
@@ -89,65 +95,87 @@ function streamTweet(user){
 
     $timelapsed.text(elapsedTime(tweet.created_at));
     $timelapsed.appendTo($tweet);
-    
+
     $message.text(tweet.message);
     $message.appendTo($tweet);
 
-      // $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + elapsedTime(tweet.created_at));
-      $tweet.css({
-        'border': '1px solid #ccc'
-      })
-      $timelapsed.css({
-        'text-align':'center'
-      })
-      
-      $tweet.appendTo($body);
-      index -= 1;
-    } 
-  }
+    // $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + elapsedTime(tweet.created_at));
+    $tweet.css({
+      'border': '1px solid #ccc'
+    });
+
+    $timelapsed.css({
+      'text-align':'center'
+    });
+    
+    $tweet.appendTo($body);
+    index -= 1;
+  } 
+};
 
 //creating list of people following
-  function streamUser(){
-    var $body = $('.people-follow');
-    for(var key in streams){
-      if(key === 'users')
-        for(var key in streams.users){
-          var user = key;
-          var $user = $('<a id='+user+'></a><br>');
-          $user.text('@' + user);
-          $user.appendTo($body);
-
-        }
-    }
-
+function streamUser() {
+  var $body = $('.people-follow');
+  for(var key in streams) {
+    if(key === 'users') {
+      for(var key in streams.users) {
+        var user = key;
+        var $user = $('<a id='+user+'></a><br>');
+        $user.text('@' + user);
+        $user.appendTo($body);
+      }
+    }  
   }
+};
 
 //display the name of the user in profile section when the user is clicked
-  function ProfileDisplay(){
-    var element = document.getElementById("user-profile");
-    if(element){
-      element.parentNode.removeChild(element);
-    }
-    var $div = $('#profile');
-    var $profile = $('<h4 id=user-profile></h4>')
-    $profile.text(event.target.id);
-    $profile.appendTo($div);
-    $profile.css({
-      'text-align':'center'
-    })
-    $profile.hide();
-    
-  };
+function ProfileDisplay() {
+  var element = document.getElementById("user-profile");
+  
+  if(element) {
+    element.parentNode.removeChild(element);
+  }
+  
+  var $div = $('#profile');
+  var $profile = $('<h4 id=user-profile></h4>');
+  $profile.text(event.target.id);
+  $profile.appendTo($div);
+  $profile.css({
+    'text-align':'center'
+  });
+
+  $profile.hide();
+};
 
 
 $(document).ready(function(){
-
+ 
   $("#new-tweet").hide();
   count = 0;
   nextTweet();
   streamTweet("home");
   streamUser();
 
+});
+
+$('#pop-up').click(function(){
+$('#myModal').modal('show');
+
+});
+
+$('#tweet-btn').click(function(){
+  var tweet = {};
+  tweet.user = 'shawndrost';
+  tweet.message = document.getElementById("user-tweet").value;
+  document.getElementById("user-tweet").value = "";
+  tweet.created_at = new Date();
+  addTweet(tweet);
+  streamTweet("home");
+  $('#myModal').modal('hide');
+})
+
+$('#close').click(function(){
+  document.getElementById("user-tweet").value = "";
 });
 
 $("#new-tweet").click(function() {
@@ -157,7 +185,7 @@ $("#new-tweet").click(function() {
 
 $(".people-follow").click(function() {
   ProfileDisplay();
-  if (event.target.id){
+  if (event.target.id) {
     streamTweet(event.target.id);
     $('#user-profile').show();
   }
@@ -165,7 +193,7 @@ $(".people-follow").click(function() {
 
 $(".tweet-container").on('click', 'a', function() {
   ProfileDisplay();
-  if (event.target.id){
+  if (event.target.id) {
     streamTweet(event.target.id);
     $('#user-profile').show();
   }
@@ -174,4 +202,4 @@ $(".tweet-container").on('click', 'a', function() {
 $('#myHome').click(function(){
   streamTweet("home");
   ProfileDisplay();
-})
+});
